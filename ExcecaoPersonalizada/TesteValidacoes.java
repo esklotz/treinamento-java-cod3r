5a4866f317f08242287f75567505008b8e7e5196
esklotz <joana.estefani@gmail.com>
2020-05-15 13:20:30 +0200

    Arquivos de treinamento excecao personalizada

---
+++ ExcecaoPersonalizada/TesteValidacoes.java
@@ -0,0 +1,22 @@
+package ExcecaoPersonalizada;
+
+import Excecao.Aluno;
+
+public class TesteValidacoes {
+	
+	public static void main(String[] args) {
+		
+		try {
+			Aluno aluno = new Aluno("Ana", -7);
+			Validar.aluno(aluno);
+		} catch (StringVaziaException | NumeroForaIntervaloException e) {
+			System.out.println(e.getMessage());
+		} 
+		
+		
+		System.out.println("Fim!");
+		
+		
+	}
+
+}
