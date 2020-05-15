e4c7641838ef6d452bc1edec865effd1cd9df1e8
esklotz <joana.estefani@gmail.com>
2020-05-15 13:21:14 +0200

    Arquivos de treinamento excecao personalizada 2

---
+++ ExcecaoPersonalizada2/NumeroForaIntervaloException.java
@@ -0,0 +1,18 @@
+package ExcecaoPersonalizada2;
+
+@SuppressWarnings("serial")
+public class NumeroForaIntervaloException extends Exception {
+	
+	private String nomeDoAtributo;
+	
+	public NumeroForaIntervaloException (String nomeDoAtributo) {
+		this.nomeDoAtributo = nomeDoAtributo;
+	}
+	
+	
+	public String getMessage() {
+		return String.format("O atributo '%s' está fora do intervalo.",
+				nomeDoAtributo);
+	}
+
+}
---
+++ ExcecaoPersonalizada2/StringVaziaException.java
@@ -0,0 +1,18 @@
+package ExcecaoPersonalizada2;
+
+@SuppressWarnings("serial")
+public class StringVaziaException extends Exception {
+	
+	private String nomeDoAtributo;
+	
+	public StringVaziaException (String nomeDoAtributo) {
+		this.nomeDoAtributo = nomeDoAtributo;
+	}
+	
+	
+	public String getMessage() {
+		return String.format("O atributo '%s' está vazio.",
+				nomeDoAtributo);
+	}
+
+}
